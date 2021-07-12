80f853338111ee7b5c7a2d93ab1a12d056580951
abbeyroyse13 <heatherruan126@gmail.com>
2021-07-12 16:02:56 -0500

    working out kinks

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,3 +1,7 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "./Home"
+
 export const ApplicationViews = () => {
     return (
         <>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const Home = () => {
     <>
         <h1>Welcome to Kandy Korner!</h1>
